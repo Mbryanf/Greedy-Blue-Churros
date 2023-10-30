6403302657194a27c95cd9bdd8870c5f51973ee5
Mark Bryan Meiras Fernandes <bryan.mark704@gmail.com>
2023-10-30 16:23:28 -0300

    Adicionando Carrossel na Home

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,30 +1,31 @@
 import React, { useState } from "react";
 import { View, StatusBar, ScrollView, Text, Image, TouchableOpacity, Linking } from 'react-native';
-import { AppBar, HStack, IconButton } from "@react-native-material/core";
-import Icon from "@expo/vector-icons/MaterialCommunityIcons";
+import { AppBar, IconButton } from '@react-native-material/core';
+import Icon from '@expo/vector-icons/MaterialCommunityIcons';
 import Modal from 'react-native-modal';
+import Carousel from 'react-native-snap-carousel';
 
 const CourseCard = ({ title, imageSource, navigation, screenName }) => (
-    <TouchableOpacity onPress={() => navigation.navigate(screenName)}>
-      <View style={{
-        margin: 10,
-        borderRadius: 10,
-        overflow: 'hidden',
-        alignItems: 'center',
-      }}>
-        <Image
-          source={imageSource}
-          style={{
-            width: 150,
-            height: 150,
-            marginBottom: 5,
-            borderRadius: 10,
-          }}
-        />
-        <Text style={{ textAlign: 'center', maxWidth: 150 }}>{title}</Text>
-      </View>
-    </TouchableOpacity>
-  );
+  <TouchableOpacity onPress={() => navigation.navigate(screenName)}>
+    <View style={{
+      margin: 15,
+      borderRadius: 10,
+      overflow: 'hidden',
+      alignItems: 'center',
+    }}>
+      <Image
+        source={imageSource}
+        style={{
+          width: 300,
+          height: 300,
+          marginBottom: 15,
+          borderRadius: 10,
+        }}
+      />
+      <Text style={{ textAlign: 'center', maxWidth: 180 }}>{title}</Text>
+    </View>
+  </TouchableOpacity>
+);
 
 export default function HomeScreen({ navigation }) {
   const appBarImage = require('../img/appbar_image.png');
@@ -49,10 +50,10 @@ export default function HomeScreen({ navigation }) {
         <IconButton icon={(props) => <Icon name="menu" {...props} />} onPress={() => setIsModalVisible(true)} />
       ),
       headerRight: () => (
-        <HStack>
+        <View style={{ flexDirection: 'row' }}>
           <IconButton icon={(props) => <Icon name="magnify" {...props} />} />
           <IconButton icon={(props) => <Icon name="dots-vertical" {...props} />} />
-        </HStack>
+        </View>
       ),
       headerStyle: {
         backgroundColor: "#0CC1EE",
@@ -60,48 +61,71 @@ export default function HomeScreen({ navigation }) {
     });
   }, [navigation]);
 
+  const carouselItems = [
+    {
+      title: 'Curso Iniciante Mobile',
+      imageSource: require('../img/image1.png'),
+      navigation: navigation,
+      screenName: 'CursoIniciante',
+    },
+    {
+      title: 'Curso Intermediário Mobile',
+      imageSource: require('../img/image2.png'),
+      navigation: navigation,
+      screenName: 'CursoIntermediario',
+    },
+    {
+      title: 'Curso de Software Mobile',
+      imageSource: require('../img/image3.png'),
+      navigation: navigation,
+      screenName: 'CursoSoftware',
+    },
+  ];
+
+  const renderCardItem = ({ item }) => {
+    const { title, imageSource, navigation, screenName } = item;
+    return (
+      <CourseCard
+        title={title}
+        imageSource={imageSource}
+        navigation={navigation}
+        screenName={screenName}
+      />
+    );
+  };
+
   return (
     <View style={{ flex: 1 }}>
       <StatusBar translucent backgroundColor="transparent" barStyle="dark-content" />
 
-      <View style={{ padding: 20, alignItems: 'center' }}>
-        <Text style={{ fontSize: 20, fontWeight: 'bold', textAlign: 'center' }}>Estude gratuitamente</Text>
-        <Text style={{ textAlign: 'center', marginTop: 10 }}>
+      <View style={{ padding: 30, alignItems: 'center' }}>
+        <Text style={{ fontSize: 30, fontWeight: 'bold', textAlign: 'center' }}>Estude gratuitamente</Text>
+        <Text style={{ fontSize: 20, textAlign: 'center', marginTop: 20 }}>
           Nós oferecemos treinamentos tanto presenciais quanto on-line para aqueles que desejam adquirir habilidades em manutenção de celulares, sejam iniciantes ou já experientes.
         </Text>
-        <Text style={{ textAlign: 'center', marginTop: 10 }}>
+        <Text style={{ fontSize: 20, textAlign: 'center', marginTop: 20 }}>
           Além disso, disponibilizamos cursos avançados para aqueles que buscam especialização em assistência técnica.
         </Text>
       </View>
 
-      <View style={{ alignItems: 'center', marginTop: 10 }}>
-        <Text style={{ fontSize: 24, fontWeight: 'bold', textAlign: 'center' }}>Nossos Cursos</Text>
+      <View style={{ alignItems: 'center', marginTop: 20 }}>
+        <Text style={{ fontSize: 30, fontWeight: 'bold', textAlign: 'center' }}>Nossos Cursos</Text>
+        <Text style={{ textAlign: 'center', marginTop: 10 }}>Arraste para o lado para ver os próximos cursos</Text>
       </View>
 
-      <ScrollView horizontal={true} style={{ marginTop: 10, paddingBottom: 0, flexGrow: 0 }} showsHorizontalScrollIndicator={false}>
-        <CourseCard
-          title="Curso Iniciante Mobile"
-          imageSource={require('../img/image1.png')}
-          navigation={navigation}
-          screenName="CursoIniciante"
-        />
-        <CourseCard
-          title="Curso Intermediário Mobile"
-          imageSource={require('../img/image2.png')}
-          navigation={navigation}
-          screenName="CursoIntermediario"
-        />
-        <CourseCard
-          title="Curso de Software Mobile"
-          imageSource={require('../img/image3.png')}
-          navigation={navigation}
-          screenName="CursoSoftware"
-        />
-      </ScrollView>
+      <Carousel
+        layout="default"
+        data={carouselItems}
+        sliderWidth={400}
+        itemWidth={300}
+        renderItem={renderCardItem}
+      />
 
-      <TouchableOpacity onPress={abrirLinkExterno} style={{ backgroundColor: '#00FF4C', padding: 20, borderRadius: 30, alignSelf: 'center', width: 250, marginTop: 20, marginBottom: 20 }}>
-        <Text style={{ color: 'white', fontWeight: 'bold', textAlign: 'center' }}>Saiba mais</Text>
-      </TouchableOpacity>
+      <View style={{ position: 'absolute', bottom: 0, width: '100%', alignItems: 'center', paddingBottom: 20 }}>
+        <TouchableOpacity onPress={abrirLinkExterno} style={{ backgroundColor: '#00FF4C', padding: 20, borderRadius: 30, width: 250, marginBottom: 20 }}>
+          <Text style={{ color: 'white', fontWeight: 'bold', textAlign: 'center' }}>Saiba mais</Text>
+        </TouchableOpacity>
+      </View>
 
       <Modal isVisible={isModalVisible} onBackdropPress={() => setIsModalVisible(false)}>
         <View style={{ backgroundColor: 'white', padding: 20, borderRadius: 10 }}>
